06ea54bbfdc13216ca8d232506af6298c95943a2
Nitheesh <Nitheesh@sixninesit.com>
2019-03-16 14:00:45 +0530

    adding

--- hello_flask/__init__.py
+++ hello_flask/__init__.py
@@ -1,7 +1,20 @@
-from flask import Flask
-
+#!/usr/bin/env python
+from flask import Flask, jsonify, request
+from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route('/')
+def view_headers():
+    d = dict(
+        headers=dict(request.headers.items()),
+        origin=request.environ['REMOTE_ADDR']
+    )
+    return jsonify(d)
+
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
